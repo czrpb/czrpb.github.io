60ef058730ca95de5a5e86e34ada93542b95b29c
Quentin Crain <czrpb00@gmail.com>
2026-02-24 18:16:35 -0800

    updated to set size on vizualization

--- ds/viz.py
+++ ds/viz.py
@@ -7,16 +7,14 @@ csv_path = sys.argv[1] if len(sys.argv) > 1 else "viz.csv"
 df = pd.read_csv(
     csv_path,
     header=None,
-    names=["Species", "Flipper Len (mm)", "Mass (g)", "Sex"]
+    names=["Species", "Flipper Len (mm)", "Mass (g)", "Sex", "Size"]
 )
 
 fig = px.scatter(
     df,
     x="Mass (g)", y="Flipper Len (mm)",
-    color="Species", symbol="Sex",
+    color="Species", symbol="Sex", size="Size",
     title="Penguin Flipper Length vs Mass"
 )
-
-fig.update_traces(marker=dict(size=12))
 
 fig.show()
